6e78cecfd21cb392d706a484650afa3c6367f8e9
Ositanachi Otugo <osi@Ositanachis-iMac.local>
2018-01-28 22:53:54 -0500

    Take 2
    Calculations work. Need to tweak UI and fix custom tip %

---
+++ tippaTests/tippaTests.swift
@@ -0,0 +1,36 @@
+//
+//  tippaTests.swift
+//  tippaTests
+//
+//  Created by Ositanachi Otugo on 1/28/18.
+//  Copyright © 2018 Ositanachi Otugo. All rights reserved.
+//
+
+import XCTest
+@testable import tippa
+
+class tippaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
